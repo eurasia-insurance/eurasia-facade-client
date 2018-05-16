23d420728e1295de8be1821317fc0550eb644d1a
Vadim.Isaev <vadim.o.isaev@gmail.com>
2018-05-16 10:03:43 +0600

    v1

---
+++ src/main/java/tech/lapsa/eurasia/facade/EJBConstants.java
@@ -0,0 +1,7 @@
+package tech.lapsa.eurasia.facade;
+
+public interface EJBConstants {
+
+    public static final String APPLICATION_NAME = "epayment-facade-app";
+    public static final String MODULE_NAME = "epayment-facade";
+}
